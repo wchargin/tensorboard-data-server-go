3c2e6068828674188c439309fe920fe1ff2a2de8
William Chargin <wchargin@gmail.com>
2020-10-19 15:36:03 -0700

    logdirloader_demo: add CPU/memory profiling flags
    For previous slices, I’ve separated a `demo` from a `bench`, but I don’t
    really see a reason to do that.

--- io/cmd/logdirloader_demo/main.go
+++ io/cmd/logdirloader_demo/main.go
@@ -3,21 +3,60 @@ package main
 import (
 	"bufio"
 	"errors"
+	"flag"
 	"fmt"
 	"io"
 	"os"
+	"runtime"
+	"runtime/pprof"
 	"sort"
 
 	"github.com/wchargin/tensorboard-data-server/fs"
 	"github.com/wchargin/tensorboard-data-server/io/logdir"
 )
 
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
+var memprofile = flag.String("memprofile", "", "write memory profile to this file")
+
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Fprintf(os.Stderr, "must specify run directory as first argument\n")
+	flag.Parse()
+	args := flag.Args()
+	if len(args) != 1 {
+		fmt.Fprintf(os.Stderr, "must specify run directory as only argument\n")
 		os.Exit(1)
 	}
-	ll := logdir.LoaderBuilder{FS: fs.OS{}, Logdir: os.Args[1]}.Start()
+
+	// Profiling structure copied from "go doc runtime/pprof".
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "creating CPU profile: %v\n", err)
+			os.Exit(1)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
+
+	readLogDirectory(args[0], len(*cpuprofile) == 0 && len(*memprofile) == 0)
+
+	if *memprofile != "" {
+		fmt.Println("creating memory profile...")
+		f, err := os.Create(*memprofile)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "creating memory profile: %v\n", err)
+			os.Exit(1)
+		}
+		defer f.Close()
+		runtime.GC()
+		if err := pprof.WriteHeapProfile(f); err != nil {
+			fmt.Fprintf(os.Stderr, "writing memory profile: %v\n", err)
+		}
+		fmt.Println("done with memory profile")
+	}
+}
+
+func readLogDirectory(dir string, poll bool) {
+	ll := logdir.LoaderBuilder{FS: fs.OS{}, Logdir: dir}.Start()
 	stdin := bufio.NewReader(os.Stdin)
 	for {
 		c := make(chan struct{})
@@ -67,6 +106,9 @@ func main() {
 			}
 		}
 
+		if !poll {
+			break
+		}
 		fmt.Printf("More (y/N)? ")
 		line, err := stdin.ReadString('\n')
 		if errors.Is(err, io.EOF) {
